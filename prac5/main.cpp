1821fc29d388497fd10fb7510e6d8162967e708e
Falwase <rishi.gogada@gmail.com>
2024-04-22 00:01:33 +0930

    fd

--- prac5/main.cpp
+++ prac5/main.cpp
@@ -3,37 +3,42 @@
 #include "RecursiveBinarySearch.h"
 
 #include <iostream>
+#include <vector>
+#include <sstream>
+#include <string>
 
-void printVect(std::vector<int> vect) {
-    for(int i = 0; i < vect.size(); i++) {
-        std::cout << vect[i] << std::endl;
-    }
-}
-
-int main() {
-
-    std::vector<int> vect;
-
-    vect.push_back(1);
-    vect.push_back(27);
-    vect.push_back(6);
-    vect.push_back(2);
-    vect.push_back(0);
-    vect.push_back(3);
-    vect.push_back(1);
-
-    QuickSort b;
-
-    //std::cout << "input vector:" << std::endl;
-    //printVect(vect);
-
-    std::cout << "sorted vector:" << std::endl;
-    printVect(b.sort(vect));
-
-    std::vector<int> orderedVect = b.sort(vect);
+void printVect(std::vector<int> list) {
 
     RecursiveBinarySearch r;
 
-    std::cout << "search result" << std::endl;
-    std::cout << r.search(orderedVect, 0) << std::endl;
+    if(r.search(list, 1)) {
+        std::cout << "true ";
+    }
+    else {
+        std::cout << "false ";
+    }
+
+    for(int i = 0; i < list.size(); i++) {
+        std::cout << list[i] << " ";
+    }
+
+    std::cout << std::endl;
+}
+
+int main() {
+    //collect input
+    std::vector<int> input;
+    std::string line;
+    std::getline(std::cin, line);
+    std::istringstream os(line);
+    int i;
+    while(os >> i) {
+        input.push_back(i);
+    }
+
+    QuickSort q;
+    std::vector<int> sortedList = q.sort(input);
+
+    printVect(sortedList);
+
 }
